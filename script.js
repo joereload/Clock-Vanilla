bd6760f9a88663d54c5aac0e2652c89b7341d120
Jose Gimenez <josegimenez@gmail.com>
2022-04-22 09:18:48 -0400

    ahora si funciona

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const secondHand = document.querySelector ("[data-second-hand]")
 function setClock () {
     const currentDate = new Date ()
     const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRadio = (secondsRatio + currentDate.getMinutes()) / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
     setRotation (secondHand, secondsRatio)
     setRotation ( minuteHand, minutesRatio)
